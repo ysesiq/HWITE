9aabca6e581118b5b7151bef9fb96f315a99ce4d
whscqngsd <112165286+whscqngsd@users.noreply.github.com>
2024-05-05 00:10:25 +0800

    fix

--- src/main/java/cn/xylose/mitemod/hwite/mixin/EntityRendererMixin.java
+++ src/main/java/cn/xylose/mitemod/hwite/mixin/EntityRendererMixin.java
@@ -3,12 +3,18 @@ package cn.xylose.mitemod.hwite.mixin;
 import cn.xylose.mitemod.hwite.HwiteMod;
 import net.minecraft.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static net.minecraft.EntityRenderer.setDebugInfoForSelectedObject;
+
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
+
+    @Shadow
+    private Minecraft mc;
 
     @Inject(method = "setDebugInfoForSelectedObject", at = @At("HEAD"))
     private static void setHwiteInfoForSelectedObject(RaycastCollision rc, EntityPlayer player, CallbackInfo ci) {
@@ -20,44 +26,64 @@ public class EntityRendererMixin {
                     HwiteMod.entityInfo = entityLivingBase;
                     float total_melee_damage;
                     if (entityLivingBase.isEntityPlayer()) {
-                        total_melee_damage = entityLivingBase.getAsPlayer().calcRawMeleeDamageVs((Entity)null, false, false);
+                        total_melee_damage = entityLivingBase.getAsPlayer().calcRawMeleeDamageVs((Entity) null, false, false);
                     } else if (entityLivingBase.hasEntityAttribute(SharedMonsterAttributes.attackDamage)) {
-                        total_melee_damage = (float)entityLivingBase.getEntityAttributeValue(SharedMonsterAttributes.attackDamage);
+                        total_melee_damage = (float) entityLivingBase.getEntityAttributeValue(SharedMonsterAttributes.attackDamage);
                     } else {
                         total_melee_damage = 0.0F;
                     }
-                    HwiteMod.info = entityLivingBase.getEntityName() + " 血量:" + entityLivingBase.getMaxHealth() + "/" + entityLivingBase.getHealth() + " 伤害:" + total_melee_damage;
+                    if (total_melee_damage == 0.0F) {
+                        HwiteMod.info = entityLivingBase.getEntityName() + " 血量:" + entityLivingBase.getHealth() + "/" + entityLivingBase.getMaxHealth();
+                        HwiteMod.info_line_1 = "";
+                    } else {
+                        HwiteMod.info = entityLivingBase.getEntityName();
+                        HwiteMod.info_line_1 = "血量:" + entityLivingBase.getHealth() + "/" + entityLivingBase.getMaxHealth() + " 伤害:" + total_melee_damage;
+                    }
                 } else {
                     HwiteMod.info = entity.getTranslatedEntityName();
                 }
 
-                HwiteMod.info_line_1 = "";
             } else if (rc.isBlock()) {
                 Block block = Block.blocksList[player.worldObj.getBlockId(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z)];
                 float block_hardness = player.worldObj.getBlockHardness(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z);
                 int metadata = player.worldObj.getBlockMetadata(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z);
                 int min_harvest_level = block.getMinHarvestLevel(metadata);
+                HwiteMod.info_line_1 = "";
+                HwiteMod.info_line_2 = "";
                 HwiteMod.info = block.getLocalizedName() + " (" + block.blockID + ":" + metadata + ")";
                 if (min_harvest_level == 0) {
-                    if(player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true) >= 0) {
-                            HwiteMod.info_line_1 = "硬度:" + block_hardness + " 有效挖掘速度:" + player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true);
-                        } else {
-                            HwiteMod.info_line_1 = "硬度:" + block_hardness + " 无法破坏";
-                        }
+                    if (player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true) == 0.0) {
+                        HwiteMod.info_line_1 = "硬度:" + block_hardness;
+                        HwiteMod.info_line_2 = EnumChatFormatting.DARK_RED + "×" + EnumChatFormatting.WHITE;
                     } else {
-                    if (player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true) >= 0) {
-                            HwiteMod.info_line_1 = "硬度:" + block_hardness + " 最低挖掘等级=" + min_harvest_level + " 有效挖掘速度:" + player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true);
-                        } else {
-                            HwiteMod.info_line_1 = "硬度:" + block_hardness + " 最低挖掘等级=" + min_harvest_level + " 无法破坏";
-                        }
+                        HwiteMod.info_line_1 = "硬度:" + block_hardness + " 挖掘速度:" + player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true);
                     }
                 } else {
+                    if (player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true) == 0.0) {
+                        HwiteMod.info_line_1 = "硬度:" + block_hardness + " 挖掘等级:" + min_harvest_level;
+                        HwiteMod.info_line_2 = EnumChatFormatting.DARK_RED + "×" + EnumChatFormatting.WHITE;
+                    } else {
+                        HwiteMod.info_line_1 = "硬度:" + block_hardness + " 挖掘等级:" + min_harvest_level;
+                        HwiteMod.info_line_2 = "挖掘速度:" + player.getCurrentPlayerStrVsBlock(rc.block_hit_x, rc.block_hit_y, rc.block_hit_z, true);
+                    }
+                }
+            } else {
                 HwiteMod.info_line_1 = "";
+                HwiteMod.info_line_2 = "";
             }
         } else {
             HwiteMod.info = "";
             HwiteMod.info_line_1 = "";
+            HwiteMod.info_line_2 = "";
         }
-
     }
+
+    @Inject(method = "getMouseOver", at = @At("TAIL"))
+    public void getMouseOver(float partial_tick, CallbackInfo ci) {
+        EntityPlayer player = (EntityPlayer)this.mc.renderViewEntity;
+        if (!Minecraft.inDevMode() || Minecraft.inDevMode()) {
+            setDebugInfoForSelectedObject(player.getSelectedObject(partial_tick, false, true, (EnumEntityReachContext) null), player);
+        }
+    }
+
 }
